c7ab4a7991d9e24ac174657f965e5e2a29609651
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2023-11-30 13:36:47 +0100

    change back

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const { getAll } = require("../front/public/js/queries");
+const { getAll } = require("./queries");
 const port = 8080;
 
 const app = express();
